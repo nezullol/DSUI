1ab3bb81af075f64b3923bff44b2a8cb6f5c3bc5
Chris Scott <mchmav@gmail.com>
2021-05-27 14:55:04 -0500

    logo and styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Logo from './logo';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Logo</h1>
-        <h1>Logo</h1>
+        <div><Logo /></div>
+        <div>Search</div>
+        <div>Recent Post</div>
       </div>
     );
   }
