cba5473188d1c54477fbce6a9417b0a47b301cd2
nezullol <mchmav@gmail.com>
2021-06-17 16:47:57 -0500

    used axios to preform get request to fetch recent posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,19 @@
+import {SET_RECENT_POSTS} from './types'
+
+import axios from 'axios';
+
+
 export function fetchRecentPosts() {
     return function(dispatch)
    { 
        //preform our requests here
-
-    console.log('hello');
+    axios.get('https://api.dailysmarty.com/posts').then(response => {
+        console.log(response.data.posts);
+        dispatch({
+            type: SET_RECENT_POSTS,
+            payload: response.data.posts
+        })
+    })
 
     }
 }
